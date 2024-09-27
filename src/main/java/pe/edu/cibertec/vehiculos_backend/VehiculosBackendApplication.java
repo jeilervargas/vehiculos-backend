83dda7c0708e76b1b125bc9fba2e663d2d4f9792
Jeiler Vargas <jeiler.a.vargas@gmail.com>
2024-09-27 18:50:55 -0500

    Commit 1: Creación del proyecto vehiculos-backend

---
+++ src/main/java/pe/edu/cibertec/vehiculos_backend/VehiculosBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.vehiculos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculosBackendApplication.class, args);
+	}
+
+}
